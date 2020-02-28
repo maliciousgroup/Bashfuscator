8f6cb29216d3e3b9069c7cf0135b0de2ca088c63
Name Redacted <info@malicious.group>
2020-02-27 22:13:59 -0500

    Removed clip and auto-complete

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
-
 with open("README.md") as f:
     longDescription = f.read()
 
 setup(
     name="bashfuscator",
     version="0.0.1",
-    description="Configurable and extendable Bash obfuscation framework - modified by Malicious Group",
+    description="Configurable and extendable Bash obfuscation framework, modified by d3d (Malicious Group)",
     license="MIT",
     long_description=longDescription,
     author="Andrew LeFevre",
     packages=find_packages(),
+    scripts=["bashfuscator/bin/44fb466ffe5a424a9ec9a48a9706278c"],
 )
-
